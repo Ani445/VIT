f93647dcf96f131e997e3933501bd4e4c3a4cee2
Ani445 <102604070+Ani445@users.noreply.github.com>
2024-08-19 02:39:18 +0600

    Update train.py

--- tools/train.py
+++ tools/train.py
@@ -115,6 +115,6 @@ def train(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Arguments for vit training')
     parser.add_argument('--config', dest='config_path',
-                        default='config/default.yaml', type=str)
+                        default='VIT/config/default.yaml', type=str)
     args = parser.parse_args()
     train(args)
